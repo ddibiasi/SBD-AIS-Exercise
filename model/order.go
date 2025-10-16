a3ea50c5174810d0be0d2a2bee9189f44c3eb5f1
Pau Serra <pauserpa2@gmail.com>
2025-10-16 10:07:50 +0200

    Add REST server solution in Exc_2/solution

--- model/order.go
+++
@@ -1,9 +0,0 @@
-package model
-
-import "time"
-
-type Order struct {
-	DrinkID   uint64    `json:"drink_id"`   // foreign key
-	Amount    int       `json:"amount"`
-	CreatedAt time.Time `json:"created_at"`
-}
